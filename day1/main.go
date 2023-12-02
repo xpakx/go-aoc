497d2d75d7aa955d21c2248f6ff11fe615f11368
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-02 19:00:28 +0100

    feat: read data for day 1

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
 	fmt.Println("Advent of Code, day 1")
+	fmt.Println("----------")
+
+	readFile, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
 }
