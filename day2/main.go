087fe4a26f494b30b62f22abf6477f00d96fc8f1
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-02 20:33:59 +0100

    feat: complete second star of day 2

--- day2/main.go
+++ day2/main.go
@@ -10,12 +10,12 @@ import (
 
 func main() {
 	first_star()
-	// second_star()
+	second_star()
 
 }
 
 func first_star() {
-	fmt.Println("Advent of Code, day 2")
+	fmt.Println("Advent of Code, day 2*")
 	fmt.Println("----------")
 
 	readFile, err := os.Open("input.txt")
@@ -74,4 +74,57 @@ func testLine(line string) (bool, int) {
 		}
 	}
 	return true, idNum
+}
+
+func second_star() {
+	fmt.Println("Advent of Code, day 2**")
+	fmt.Println("----------")
+
+	readFile, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+	var result int = 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		red, green, blue := getMax(line)
+		power := red*green*blue
+		result += power
+	}
+	fmt.Println(result)
+
+	readFile.Close()
+}
+
+func getMax(line string) (int, int, int) {
+	maxRed := 0
+	maxGreen := 0
+	maxBlue := 0
+	splitLine := strings.Split(line, ":")
+	sets := strings.Split(splitLine[1], ";")
+	for _, set := range sets {
+		stones := strings.Split(set, ",")
+		for _, stone := range stones {
+			stoneTrim := strings.TrimSpace(stone)
+			pair := strings.Split(stoneTrim, " ")
+			color := pair[1]
+			value, _ := strconv.Atoi(pair[0])
+			if color == "red" && value > maxRed {
+				maxRed = value
+			}
+			if color == "green" && value > maxGreen {
+				maxGreen = value
+			}
+			if color == "blue" && value > maxBlue {
+				maxBlue = value
+			}
+		}
+	}
+	return maxRed, maxGreen, maxBlue
 }
