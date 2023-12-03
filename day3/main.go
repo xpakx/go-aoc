3b7130bc14af557f2fb0e9a2c6a6ff75b9441e60
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-03 16:17:56 +0100

    feat(day3): calculate result

--- day3/main.go
+++ day3/main.go
@@ -26,6 +26,14 @@ func solve() int {
 	fileScanner := bufio.NewScanner(readFile)
 
 	fileScanner.Split(bufio.ScanLines)
+	last := make([]bool, 140)
+	currLine := make([]bool, 140)
+	for i := range last {
+		last[i] = false
+	}
+	numbersLast := make([]Number, 0)
+	numbers := make([]Number, 0)
+	result := 0
 
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
@@ -44,15 +52,71 @@ func solve() int {
 			} else if current > 0 {
 				num := Number{current, start, i-1}
 				current = 0
-				fmt.Print(num.value)
-				fmt.Print("(", num.start, "-", num.end, ")")
-				fmt.Print(", ")
+				numbers = append(numbers, num)
+			}
+			if (char > 57 || char < 48) && char != 46 {
+				currLine[i] = true
+			} else {
+				currLine[i] = false
 			}
 		}
-		fmt.Println()
+		fmt.Println("last")
+		for _, num := range numbersLast {
+			start := num.start
+			if num.start > 0 {
+				start -= 1
+			}
+			end := num.end
+			if num.end+1 < len(chars) {
+				end += 1
+			}
+			test := false
+			for i:=start; i<=end; i++ {
+				if currLine[i] {
+					test = true
+				}
+			}
+			if test {
+				result += num.value
+				fmt.Println("added", num.value)
+			}
+		}
+		numbersLast = nil
+		fmt.Println("current")
+
+		for _, num := range numbers {
+			start := num.start
+			if num.start > 0 {
+				start -= 1
+			}
+			end := num.end
+			if num.end+1 < len(chars) {
+				end += 1
+			}
+			test := false
+			for i:=start; i<=end; i++ {
+				if last[i] {
+					test = true
+				}
+			}
+
+			if test || (num.start > 0 && currLine[num.start-1]) || (num.end+1 < len(chars) && currLine[num.end+1]) {
+				result += num.value
+				fmt.Println("added", num.value)
+
+			} else {
+				numbersLast = append(numbersLast, num)
+			}
+
+		}
+		numbers = nil
+		
+		for i := range last {
+			last[i] = currLine[i]
+		}
 	}
 	readFile.Close()
-	return 0
+	return result
 }
 
 type Number struct {
