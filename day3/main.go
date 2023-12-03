742bca17191ebca67eb07d445862fe7d716f98a5
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-03 15:25:00 +0100

    feat(day3): parse numbers

--- day3/main.go
+++ day3/main.go
@@ -30,7 +30,33 @@ func solve() int {
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		fmt.Println(line)
+		chars := []int32(line)
+		current := 0
+		start := 0
+		for i, char := range chars {
+			if char <= 57 && char >= 48 {
+				if current == 0 {
+					start = i
+				}
+				digit := int(char-48)
+				current *= 10
+				current += digit
+			} else if current > 0 {
+				num := Number{current, start, i-1}
+				current = 0
+				fmt.Print(num.value)
+				fmt.Print("(", num.start, "-", num.end, ")")
+				fmt.Print(", ")
+			}
+		}
+		fmt.Println()
 	}
 	readFile.Close()
 	return 0
+}
+
+type Number struct {
+	value int
+	start int
+	end int
 }
