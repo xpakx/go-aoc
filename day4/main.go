93c3bbdf131d57c012d2db9912fba0ac1a82539e
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-04 12:42:28 +0100

    feat(day4): calculate result

--- day4/main.go
+++ day4/main.go
@@ -28,33 +28,14 @@ func solve() int {
 
 	fileScanner.Split(bufio.ScanLines)
 	result := 0
-	reg := regexp.MustCompile("Card (?P<Id>\\d+): (?P<Winning>.*) \\| (?P<Numbers>.*)")
 
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		match := reg.FindStringSubmatch(line)
-		id := match[1]
-		idValue, _ := strconv.Atoi(id)
-		winning := match[2]
-		numbers := match[3]
-		winning = strings.Join(strings.Fields(winning), " ")
-		winningList := strings.Split(winning, " ")
-		winningFin := []int{}
-		for _, x := range winningList {
-			n, _ := strconv.Atoi(x)
-			winningFin = append(winningFin, n)
-		}
-		numbers = strings.Join(strings.Fields(numbers), " ")
-		numbersList := strings.Split(numbers, " ")
-		numbersFin := []int{}
-		for _, x := range numbersList {
-			n, _ := strconv.Atoi(x)
-			numbersFin = append(numbersFin, n)
-		}
-		numbers = strings.Join(strings.Fields(numbers), " ")
-		game := Game{idValue, numbersFin, winningFin}
+		game := ParseLine(line)
 		fmt.Printf("%#v\n", game)
-
+		common := Intersection(game.numbers, game.winning)
+		fmt.Println(common)
+		result += CalculatePoints(len(common))
 	}
 	readFile.Close()
 	return result
@@ -64,4 +45,52 @@ type Game struct {
 	id int
 	numbers []int
 	winning []int
+}
+
+func ParseLine(line string) Game {
+	reg := regexp.MustCompile("Card (?P<Id>\\d+): (?P<Winning>.*) \\| (?P<Numbers>.*)")
+	match := reg.FindStringSubmatch(line)
+	id, _ := strconv.Atoi(match[1])
+	winning := ParseList(match[2])
+	numbers := ParseList(match[3])
+	return Game{id, numbers, winning}
+}
+
+func ParseList(list string) []int {
+	listWithoutDuplicatedSpaces := strings.Join(strings.Fields(list), " ")
+	listOfStrings := strings.Split(listWithoutDuplicatedSpaces, " ")
+	listFin := []int{}
+	for _, x := range listOfStrings {
+		n, _ := strconv.Atoi(x)
+		listFin = append(listFin, n)
+	}
+	return listFin
+}
+
+func Intersection(list []int, list2 []int) []int {
+	set := make([]int, 0)
+	hash := make(map[int]struct{})
+
+	for _, num := range list {
+		hash[num] = struct{}{}
+	}
+
+	for _, num := range list2 {
+		if _, ok := hash[num]; ok {
+			set = append(set, num)
+		}
+	}
+
+	return set
+}
+
+func CalculatePoints(numbers int) int {
+	if numbers <= 1 {
+		return numbers
+	} 
+	value := 1
+	for i:=1; i<numbers; i++ {
+		value *= 2
+	}
+	return value
 }
