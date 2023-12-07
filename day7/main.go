b40aa19d950da6d1937d1d682463a873ca5eff46
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-07 12:56:03 +0100

    feat(day7): check hand type

--- day7/main.go
+++ day7/main.go
@@ -26,6 +26,7 @@ func solve(lines []string) int {
 		}
 		fmt.Println()
 		fmt.Println("Bid: ", hand.bid)
+		fmt.Println("Type: ", hand.PrintableHandType())
 	}
 	return 0
 }
@@ -63,7 +64,47 @@ func ParseHand(line string) Hand {
 	splitted := strings.Split(line, " ")
 	cards := strings.Split(splitted[0], "")
 	bid, _ := strconv.Atoi(splitted[1])
-	return Hand{cards, bid}
+	handType := CalculateHandType(cards)
+	return Hand{cards, bid, handType}
+}
+
+func CalculateHandType(cards []string) int {
+	hash := make(map[string]int)
+	for _, card := range cards {
+		if _, ok := hash[card]; ok {
+			hash[card] += 1
+		} else {
+			hash[card] = 1
+		}
+	}
+	values := make([]int, 6)
+	for i := range values {
+		values[i] = 0
+	}
+
+	for i := range hash {
+		values[hash[i]] += 1
+	}
+
+	if values[5] == 1 {
+		return Five
+	}
+	if values[4] == 1 {
+		return Four
+	}
+	if values[3] == 1 && values[2] == 1 {
+		return House
+	}
+	if values[3] == 1 {
+		return Three
+	}
+	if values[2] == 2 {
+		return TwoPairs
+	}
+	if values[2] == 1 {
+		return Pair
+	}
+	return HighCard
 }
 
 const (
@@ -79,5 +120,18 @@ const (
 type Hand struct {
 	cards []string
 	bid int
-	// result int
+	handType int
+}
+
+func (hand Hand) PrintableHandType() string {
+	cards := map[int]string{
+		HighCard: "High card",
+		Pair: "Pair",
+		TwoPairs: "Two pairs",
+		Three: "Three of a kind",
+		House: "Full house",
+		Four: "Four of a kind",
+		Five: "Five of a kind",
+	}
+	return cards[hand.handType]
 }
