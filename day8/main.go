3ba31eb3527b991c4deb7f6eb41e4cf5a719b973
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-08 11:25:57 +0100

    feat(day8): parse input

---
+++ day8/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"regexp"
+)
+
+func main() {
+	fmt.Println("Advent of Code, day 8")
+	fmt.Println("=====================")
+	directions, nodes := GetInput("input.txt")
+	first := solve(directions, nodes)
+	fmt.Print("*  ")
+	fmt.Println(first)
+}
+
+func solve(directionString string, nodeList []string) int {
+	nodes := ParseNodes(nodeList)
+	directions := ParseDirections(directionString)
+	for _, node := range nodes {
+		fmt.Println("Node", node.name)
+		fmt.Println(node.left, "<- ->", node.right)
+		fmt.Println()
+	}
+	for _, dir := range directions {
+		if dir {
+			fmt.Print("Left ")
+		} else {
+			fmt.Print("Right ")
+		}
+	}
+	fmt.Println()
+	nodeMap := constructMap(nodes)
+	fmt.Println(nodeMap)
+	return 0
+}
+
+func GetInput(filename string) (string, []string) {
+	readFile, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+	var directions string
+	var nodes []string
+	i := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		if i == 0 {
+			directions = line
+		}
+		if i > 1 {
+			nodes = append(nodes, line)
+		}
+		i++;
+	}
+	readFile.Close()
+	return directions, nodes
+}
+
+func ParseNodes(lines []string) []Node {
+	var nodes []Node
+	for _, line := range lines {
+		node := ParseNode(line)
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+func ParseNode(line string) Node {
+	reg := regexp.MustCompile("([A-Z]+) = \\(([A-Z]+), ([A-Z]+)\\)$")
+	match := reg.FindStringSubmatch(line)
+	name := match[1]
+	left := match[2]
+	right := match[3]
+	return Node{name, left, right}
+}
+
+func ParseDirections(line string) []bool {
+	chars := []int32(line)
+	result := make([]bool, 0)
+	left := int32('L');
+	right := int32('R');
+	for _, char := range chars{
+		if char == left {
+			result = append(result, true)
+		} else if char == right {
+			result = append(result, false)
+		}
+
+	}
+	return result
+}
+
+func constructMap(nodes []Node) map[string]Node {
+	hash := make(map[string]Node)
+	for _, node := range nodes {
+		hash[node.name] = node
+	}
+	return hash
+}
+
+type Node struct {
+	name string
+	left string
+	right string
+}
