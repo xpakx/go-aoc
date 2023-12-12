9d1f1f51b888b7e713705b14360b62ef89bdb80f
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-12 22:36:10 +0100

    feat(day12): solve second star

--- day12/main.go
+++ day12/main.go
@@ -11,12 +11,14 @@ import (
 func main() {
 	fmt.Println("Advent of Code, day 12")
 	fmt.Println("=====================")
-	first := SolveFirst("input.txt")
+	input := Parse("input.txt")
 	fmt.Print("*  ")
-	fmt.Println(first)
+	fmt.Println(Solve(input, false))
+	fmt.Print("** ")
+	fmt.Println(Solve(input, true))
 }
 
-func SolveFirst(filename string) int {
+func Parse(filename string) []Row {
 	readFile, err := os.Open(filename)
 
 	if err != nil {
@@ -27,76 +29,51 @@ func SolveFirst(filename string) int {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	result := 0
+	result := make([]Row, 0)
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		row :=  ParseLine(line);
-		fmt.Println(row)
-		result += row
+		result = append(result, ParseLine(line))
 	}
 	readFile.Close()
 	return result
 }
 
-func ParseLine(line string) int {
-	fmt.Println("New Line")
-	fmt.Println(line)
-	values := strings.Split(line, " ")
-	rows := values[0]
-	vals := ParseList(values[1])
-
-	return TryToPlace(rows, vals)
+type Row struct {
+	row string
+	values []int
 }
 
-func TryToPlace(rows string, vals []int) int {
-	possibilities := make([]int, 0)
-	possibilities = append(possibilities, 0)
-
-	for _, v := range vals {
-		newPossibilities := make([]int, 0)
-		for _, start := range possibilities {
-			i := start
-			for i<len(rows) && rows[i] != '#' {
-				if TestPlacement(rows, i, v) {
-					newPossibilities = append(newPossibilities, i+v-1+2)
-				}
-				i++
-			}
-			if TestPlacement(rows, i, v) {
-				newPossibilities = append(newPossibilities, i+v-1+2)
-			}
-		}
-		possibilities = nil
-		possibilities = append(possibilities, newPossibilities...)
-	}
+func Solve(input []Row, second bool) int {
 	result := 0
-	for _, p := range possibilities {
-		if TestOmittedPlaces(rows, p) {
-			result++
+	for i := range input {
+		posMap := make(map[Pos]int, 0)
+		if second {
+			row := input[i].row
+			row = row + "?" + row + "?" + row + "?" + row + "?" + row
+			old := input[i].values
+			vals := make([]int, 5*len(old))
+			for j := range old {
+				vals[j] = old[j]
+				vals[j+len(old)] = old[j]
+				vals[j+2*len(old)] = old[j]
+				vals[j+3*len(old)] = old[j]
+				vals[j+4*len(old)] = old[j]
+			}
+			result += TryToPlaceRec(posMap, row, vals, 0, 0, 0)
+		} else {
+			result += TryToPlaceRec(posMap, input[i].row, input[i].values, 0, 0, 0)
 		}
 	}
-
 	return result
 }
 
-func TestPlacement(row string, start int, length int) bool {
-	if start + length - 1 >= len(row) {
-		return false
-	}
-	for i:=start; i<start+length; i++ {
-		if row[i] == '.' {
-			return false
-		}
-	}
-	if start+length<len(row) && row[start+length] == '#' {
-		return false
-	}
-	if start>0 && row[start-1] == '#' {
-		return false
-	}
-	return true
-}
+func ParseLine(line string) Row {
+	values := strings.Split(line, " ")
+	rows := values[0]
+	vals := ParseList(values[1])
+	return Row{rows, vals}
 
+}
 
 func ParseList(list string) []int {
 	splitted := strings.Split(list, ",")
@@ -108,11 +85,38 @@ func ParseList(list string) []int {
 	return listFin
 }
 
-func TestOmittedPlaces(row string, end int) bool {
-	for i:=end; i<len(row); i++ {
-		if row[i] == '#' {
-			return false
+type Pos struct {
+	pos int
+	value int
+	found int
+}
+
+func TryToPlaceRec(hash map[Pos]int, row string, values []int, pos int, value int, found int) int {
+	key := Pos{pos, value, found}
+	if value, ok := hash[key]; ok {
+		return value
+	}
+	if pos == len(row) {
+		if value == len(values) && found == 0 {
+			return 1
+		} else if value == len(values)-1 && values[value]== found {
+			return 1
+		} else {
+			return 0
 		}
 	}
-	return true
+
+	result := 0
+	if row[pos] == '.' || row[pos] == '?' {
+		if found == 0 {
+			result += TryToPlaceRec(hash, row, values, pos+1, value, 0)
+		} else if found > 0 && value<len(values) && values[value]==found {
+			result += TryToPlaceRec(hash, row, values, pos+1, value+1, 0)
+		}
+	}
+	if row[pos] == '#' || row[pos] == '?' {
+		result += TryToPlaceRec(hash, row, values, pos+1, value, found+1)
+	}
+	hash[key] = result
+	return result
 }
