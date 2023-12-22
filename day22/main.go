f1dbea882173420a2f11fa22d44e86e8e35d3d6c
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-22 23:36:37 +0100

    fix(day22): add additional check for points

--- day22/main.go
+++ day22/main.go
@@ -98,26 +98,23 @@ func Intersect(b1, b2 Block) bool {
 	y3 := b2.coordA.y
 	x4 := b2.coordB.x
 	y4 := b2.coordB.y
-	fmt.Println(b1.coordA, b1.coordB, b2.coordA, b2.coordB)
 
 	if x1 == x2 && y1 == y2 {
-		fmt.Println("first is point")
+		if x3 == x4 && y3 == y4 {
+			return x1 == x3 && y1 == y3
+		}
 		return PointOnLine(x1, y1, x3, y3, x4, y4) 
 	}
 
 	if x3 == x4 && y3 == y4 {
-		fmt.Println("second is point")
 		return PointOnLine(x3, y3, x1, y1, x2, y2) 
 	}
 
 	denominator := (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
 	if denominator == 0 {
-		
 		if (BetweenI(x1, x3, x4) && BetweenI(y1, y3, y4)) || (BetweenI(x2, x3, x4) && BetweenI(y2, y3, y4)) {
-			fmt.Println("true, parallel, but one point inside ")
 			return true
 		}
-		fmt.Println("false, parallel")
 		return false
 	}
 
@@ -128,11 +125,9 @@ func Intersect(b1, b2 Block) bool {
 	y0 := float64(b)/float64(denominator)
 
 	if Between(x0, x1, x2) && Between(y0, y1, y2) && Between(x0, x3, x4) && Between(y0, y3, y4) {
-		fmt.Println("true, inbetween")
 		return true
 	}
 	
-	fmt.Println("fin false")
 	return false
 }
 
@@ -186,43 +181,36 @@ func SolveFirst(blocks map[int][]Block) int {
 	supportedBy := make(map[Block][]Block, 0)
 
 	for z:=1; z<=maxZ; z++ {
-		if zBlocks, ok := blocks[z]; ok {
-			for _, block := range zBlocks {
-				changed := false
-				for i:=z-1; i>=0; i-- {
-					if i == 0 {
-						block = block.MoveToZ(1)
+		for _, block := range blocks[z] {
+			changed := false
+			for i:=z-1; i>=0; i-- {
+				if i == 0 {
+					block = block.MoveToZ(1)
+					changed = true
+				} 
+
+				for _, b := range stopped[i] {
+					if Intersect(b, block) {
+						block = block.MoveToZ(i+1)
+						supports[b] = append(supports[b], block)
+						supportedBy[block] = append(supportedBy[block], b)
 						changed = true
-					} 
-
-					for _, b := range stopped[i] {
-						if Intersect(b, block) {
-							block = block.MoveToZ(i+1)
-							supports[b] = append(supports[b], block)
-							supportedBy[block] = append(supportedBy[block], b)
-							changed = true
-						}
 					}
+				}
 
-					if changed {
-						currZ := Max(block.coordA.z, block.coordB.z)
-						stopped[currZ] = append(stopped[currZ], block)
-						break
-					}
+				if changed {
+					currZ := Max(block.coordA.z, block.coordB.z)
+					stopped[currZ] = append(stopped[currZ], block)
+					break
 				}
 			}
 		}
 	}
 	result := 0
 	for _, level := range stopped {
-		// fmt.Println("level", i)
 		for _, block := range level {
-			// fmt.Println(block.coordA, block.coordB, len(supports[block]), len(supportedBy[block]))
-			// fmt.Println("supports", supports[block])
-			// fmt.Println("supportedBy", supportedBy[block])
 			if len(supports[block]) == 0 {
 				result++
-				// fmt.Println("disintegrate")
 			} else {
 				ok := true
 				for _, b := range supports[block] {
@@ -232,7 +220,6 @@ func SolveFirst(blocks map[int][]Block) int {
 					}
 				}
 				if ok {
-					// fmt.Println("disintegrate")
 					result++
 				}
 			}
