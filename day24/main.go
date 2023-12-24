f00f1a7be075aa707740f6fb1f591a52e870307a
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-24 13:39:17 +0100

    feat(day24): solve first star

--- day24/main.go
+++ day24/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -35,7 +36,7 @@ func Parse(filename string) []Line {
 		points := strings.Split(line, " @ ")
 		p := ParseList(points[0])
 		v := ParseList(points[1])
-		board = append(board, Line{Pos{p[0], p[1], p[2]}, Pos{p[0]+v[0], p[1]+v[1], p[2]+v[2]}})
+		board = append(board, Line{Pos{p[0], p[1], p[2]}, Pos{v[0], v[1], v[2]}})
 	}
 	readFile.Close()
 	return board
@@ -58,38 +59,40 @@ const (
 )
 
 func (line Line) UpwardX() bool {
-	return line.coordA.x < line.coordB.x
+	return !math.Signbit(float64(line.vector.x))
 }
 
 func (line Line) UpwardY() bool {
-	return line.coordA.y < line.coordB.y
+	return !math.Signbit(float64(line.vector.y))
 }
 
 func (line Line) InThePast(intersection Intersection) bool {
 	xInThePast := false
+	epsilon := 1E-03
 	if line.UpwardX() {
-		xInThePast = intersection.x < float64(line.coordA.x)
+		xInThePast = intersection.x < float64(line.start.x)+epsilon
 	} else {
-		xInThePast = intersection.x > float64(line.coordA.x)
+		xInThePast = intersection.x > float64(line.start.x)-epsilon
 	}
 	yInThePast := false
 	if line.UpwardY() {
-		yInThePast = intersection.y < float64(line.coordA.y)
+		yInThePast = intersection.y < float64(line.start.y)+epsilon
 	} else {
-		yInThePast = intersection.y > float64(line.coordA.y)
+		yInThePast = intersection.y > float64(line.start.y)-epsilon
 	}
 	return xInThePast || yInThePast
 }
 
 func Solve(lines []Line) int {
 	result := 0
+	epsilon := 1E-03
 	for i:=0; i<len(lines)-1; i++{
 		for j:=i+1; j<len(lines); j++ {
-			intersection := Intersect(lines[i].coordA, lines[i].coordB, lines[j].coordA, lines[j].coordB)
+			intersection := IntersectSlope(lines[i], lines[j])
 			if intersection.x == -1 {
 				continue
 			}
-			if intersection.x < MIN || intersection.y < MIN || intersection.x > MAX || intersection.y > MAX  {
+			if intersection.x < MIN+epsilon || intersection.y < MIN+epsilon || intersection.x > MAX-epsilon || intersection.y > MAX-epsilon  {
 				continue
 			}
 			if lines[i].InThePast(intersection) {
@@ -111,8 +114,8 @@ type Pos struct {
 }
 
 type Line struct {
-	coordA Pos
-	coordB Pos
+	start Pos
+	vector Pos
 }
 
 type Intersection struct {
@@ -120,15 +123,28 @@ type Intersection struct {
 	y float64
 }
 
-func Intersect(a1, a2, b1, b2 Pos) Intersection {
-	x1 := a1.x
-	y1 := a1.y
-	x2 := a2.x
-	y2 := a2.y
-	x3 := b1.x
-	y3 := b1.y
-	x4 := b2.x
-	y4 := b2.y
+func IntersectSlope(l, m Line) Intersection {
+	epsilon := 1E-03
+	slopeL := float64(l.vector.y)/float64(l.vector.x)
+	slopeM := float64(m.vector.y)/float64(m.vector.x)
+	if slopeL >= slopeM - epsilon && slopeL <= slopeM + epsilon {
+		return Intersection{-1, -1}
+	}
+
+	x0 := (float64(l.start.y)-float64(m.start.y)  - float64(l.start.x)*slopeL + float64(m.start.x)*slopeM)/(slopeM-slopeL)
+	y0 := float64(l.start.y) + slopeL*(x0-float64(l.start.x))
+	return Intersection{x0, y0}
+}
+
+func Intersect(l, m Line) Intersection {
+	x1 := l.start.x
+	y1 := l.start.y
+	x2 := l.start.x+l.vector.x
+	y2 := l.start.y+l.vector.y
+	x3 := m.start.x
+	y3 := m.start.y
+	x4 := m.start.x+m.vector.x
+	y4 := m.start.y+m.vector.y
 
 	denominator := (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
 	if denominator == 0 {
